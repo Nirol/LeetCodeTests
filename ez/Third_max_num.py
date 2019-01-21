a57dd01831a9dedd0e4e34141b03a55bb78b9ddf
Bukagg <nirolgg@gmail.com>
2019-01-21 10:43:15 +0200

    edited file for PEP8

--- ez/Third_max_num.py
+++ ez/Third_max_num.py
@@ -1,44 +1,33 @@
 class Solution(object):
-    def thirdMax(self, nums):
+    @staticmethod
+    def third_max(nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        max= float("-inf")
-        sec=float("-inf")
-        third=float("-inf")
+        first_max = float("-inf")
+        sec = float("-inf")
+        third = float("-inf")
         for n in nums:
             if n > third:
-                if n > max:
-                    third=sec
-                    sec=max
-                    max=n
-                elif n > sec and n != max:
+                if n > first_max:
+                    third = sec
+                    sec = first_max
+                    first_max = n
+                elif n > sec and n != first_max:
                     third = sec
                     sec = n
-                elif n!=sec and n!=max:
+                elif n != sec and n != first_max:
                     third = n
-            print("max = %d sec = %d third = %d",max,sec,third)
+            print("first_max = %d sec = %d third = %d", first_max, sec, third)
 
-
-        if max == sec or sec == third or third == float("-inf"):
-            return max
+        if first_max == sec or sec == third or third == float("-inf"):
+            return first_max
         else:
             return third
 
 
-
-
-
-
-
-
-
-
-
-if(__name__ == "__main__"):
-    x = Solution()
-    flowerbed =[1]
-
-    ans = x.thirdMax(flowerbed)
-    print (ans)
+if __name__ == "__main__":
+    flowerbed = [1]
+    ans = Solution.third_max(flowerbed)
+    print(ans)
